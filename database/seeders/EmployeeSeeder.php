e0d2f082db7e40c2b2cdea3a2f6b8d8b8f722bb2
adityaeks <mohadityaekosaputra11@gmail.com>
2024-06-26 17:49:38 +0700

    first commit

---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Employee;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    w
+}
